bca049bc7459965e5f4764d1f49c20df02282eba
Muhammad Kumail <mkumailofficial@gmail.com>
2020-05-03 05:50:44 +0500

    required few chanegs

--- Associations/embed.js
+++ Associations/embed.js
@@ -2,10 +2,51 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose
-mafmsf
-fsaf
-fmslfm
-flsmf
-faf;cancelAnimationFramemfmsf
-ff
+mongoose.connect('mongodb://localhost/blog_demo');
+
+var userSchema = mongoose.Schema({
+    name: String, 
+    email: String
+});
+
+var User = mongoose.model('User', userSchema);
+
+//creating a post schema
+var postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+
+var Post = mongoose.model('Post', postSchema);
+
+var newUser = new User({
+    email: 'charlie@brown.edu',
+    name: "Charlie Brown"
+});
+
+newUser.save(function(err, user){
+    if(err){
+        console.log(err);
+    } else{
+        console.log(user);
+    }
+});
+
+var newPost = new Post({
+    title: "Refelctions on apples",
+    content: "they are delicious"
+});
+
+newPost.save(function(err, post){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(post);        
+    }
+});
+
+
+
+
+
